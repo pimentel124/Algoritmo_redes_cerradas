4b208a804736f46a8ba915a7593742fae647e8bb
Álvaro Pimentel <alvaropimentel124@gmail.com>
2022-05-12 12:21:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Mediante este script se implementará el algoritmo de colas cerradas y se representará el resultado en una gráfica.
+"""
+
+
+def main():
+    print("Inserte razones de visita separadas por comas:")
+    Vi = input().split(",")
+    print("Inserte tiempos de servicios separados por comas:")
+    Si = input().split(",")
+    print("Inserte n:")
+    nmax = int(input())
+    Ni = len(Vi)
+    Ni.append(0)
+
+    counter = 1
+    while counter<=nmax:
+        Ni.append()
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
