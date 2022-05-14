a5f4696f12c22f385106c18b50aeb8b52cdf59e2
Álvaro Pimentel <alvaropimentel124@gmail.com>
2022-05-14 13:59:09 +0200

    Buena pinta

--- main.py
+++ main.py
@@ -2,51 +2,65 @@
 """
 Mediante este script se implementará el algoritmo de colas cerradas y se representará el resultado en una gráfica.
 """
+from typing import List
 
 
 def main():
+    global RVisita, Qi, TServicio, Respuesta, ZReflexion
+
     print("Inserte razones de visita separadas por comas:")
-    Vi = input().split(",")
+
+    RVisita = input().split(",")
     print("Inserte tiempos de servicios separados por comas:")
-    Si = input().split(",")
+    TServicio = input().split(",")
+    dispositivos = len(RVisita)
+
     print("Inserte N:")
     nmax = int(input())
     print("Inserte Z:")
-    Z = int(input())
+    ZReflexion = int(input())
 
     # The device response time
     # Ri(N) = Si (1+Qi(N-1))
-    Ri = [0]
+    Respuesta = [len(RVisita)][0]
 
     # The device queue lengths Qi(n) == Ni(n) with N jobs in the network using Little's law are:
     # Qi(n) = X(n) * Vi * Ri(n)
-    Qi = [0]
+    Qi = [len(RVisita)][0]
 
     # R(n) = Sum(i=1,K)Vi * Ri(n)
+    R = [0]
 
     # X(n) = n / Z + R(n)
 
+    X = [0]
+
     # The device queue lengths Ni(n) with N jobs in the network using Little's law are:
 
-    Xi = [0]
-    Ui = [0]
+    Xi = [len(RVisita)][0]
+    Ui = [len(RVisita)][0]
 
-    Qi.append(0)
+    for i in range(dispositivos):
+        Qi[i].append(0)
+    users = 0
 
+    users += 1
 
-    counter = 1
-    while counter<=nmax:
-        Ni.append()
+    for users in range(nmax):
 
+        for i in range(dispositivos):
+            Respuesta[i].append(TServicio[i] * (1 + Qi[i][users - 1]))
 
+        sum = 0
+        for i in range(dispositivos):
+            sum += Respuesta[i] * RVisita[i]
+        R.append(sum)
 
+        X.append(users / (ZReflexion + R[len(R)]))
 
-
-
-
+        for i in range(dispositivos):
+            Qi[i].append(X[i] * Respuesta[i][0] * RVisita[i])
 
 
 if __name__ == '__main__':
     main()
-
-
